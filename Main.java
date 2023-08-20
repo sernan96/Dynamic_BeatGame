c1d9744091e338d9d1d7d8172f8f08b1b33cb060
Hwang gend <80672346+sernan96@users.noreply.github.com>
2023-08-20 16:30:14 +0900

    메인 파일

---
+++ Main.java
@@ -0,0 +1,12 @@
+package dynamic_beat_1;
+
+public class Main {
+	
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_Height = 720;
+	
+	public static void main(String [] args) {
+		new DynamicBeat();
+		
+	}//이미지를 그냥 불러오면 버퍼링 현상 심해서 더블버퍼링이란 프로그래밍기법 사용필요
+}
